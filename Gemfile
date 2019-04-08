e5c74bbe5d6e04ca421b59d46ed40d1460f468fd
roni talvi <vtalvi@gmail.com>
2019-04-08 16:33:58 +0300

    created middleman project

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+ruby "2.6.2"
+
+gem "bourbon"
+gem "builder"
+gem "middleman"
+gem "middleman-aria_current"
+gem "middleman-inline_svg"
+gem "middleman-autoprefixer"
+gem "middleman-minify-html"
+gem "neat"
+gem "redcarpet"
+gem "sassc"
---
+++ source/assets/javascripts/application.js
@@ -0,0 +1,55 @@
+ $(function () {
+
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 2000,
+        maxwidth: 800,
+        namespace: "transparent-btns"
+      });
+ });
+
+
+
+
+  function scrollWin(x, y) {
+  window.scrollBy(x, y);
+}
+
+
+var typeString = [ 'the authentic nature…'];
+      var  i = 0;
+      var count = 0
+      var selectedText = '';
+      var text = '';
+      (function type() {
+        if (count == typeString.length) {
+          count = 0;
+        }
+        selectedText = typeString[count];
+        text = selectedText.slice(0, ++i);
+        document.getElementById('typing').innerHTML = text;
+        if (text.length === selectedText.length) {
+          count++;
+          i = 0;
+        }
+        setTimeout(type, 200);
+      }());
+
+      function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
+        }
+      }
+
+
+
+
+ $('.parallax-window').parallax({imageSrc: '../images/colombia.png'});
+
+
